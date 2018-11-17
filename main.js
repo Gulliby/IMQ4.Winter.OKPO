2bc43e6a467b65786740c2b296b6aec739d4c32e
Gulliby <ilyamyalik20@gmail.com>
2018-11-17 15:21:22 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
-function uploadFile(event) {
+function uploadFile(event, type) {
     var file = event.target.files[0];
     var reader = new FileReader();
-
-    reader.onload = fileUploadEnd;
+    if (type === 'pie') {
+        reader.onload = filePieUploadEnd;
+    } else {
+        reader.onload = fileLineUploadEnd;
+    }
     
     reader.fileName = getFileNameWithoutExtension(file.name);
     reader.readAsText(file);
@@ -30,7 +33,7 @@ function getFileNameWithoutExtension(fileName) {
     return fileName.split('.').slice(0, -1).join('.');
 }
 
-function fileUploadEnd(event) {
+function filePieUploadEnd(event) {
     var arrayDataResult = [];
     var parseResult = Papa.parse(event.target.result);
     for(var i = 1; i < parseResult.data.length; i++) {
@@ -66,5 +69,74 @@ function configureDefaultPlotOptions() {
                 }
             }
         }
+    };
+}
+
+function fileLineUploadEnd(event) {
+    var arrayDataResult = [];
+    var parseResult = Papa.parse(event.target.result);
+    for(var i = 1; i < parseResult.data.length; i++) {
+        arrayDataResult.push([parseInt(parseResult.data[i][0]), parseInt(parseResult.data[i][1])]);
+    }
+
+    initLineChart(arrayDataResult, event.target.fileName, parseResult.data[0][0], parseResult.data[0][1]);
+}
+
+function initLineChart(dataToShow, fileName, xName, yName) {
+    Highcharts.chart('container-line', {
+        chart: configureLineChartType(),
+        title: {
+            text: fileName
+        },
+        xAxis: {
+            reversed: false,
+            title: {
+                enabled: true,
+                text: xName
+            },
+            labels: {
+                format: '{value} km'
+            },
+            maxPadding: 0.05,
+            showLastLabel: true
+        },
+        yAxis: {
+            title: {
+                text: yName
+            },
+            labels: {
+                format: '{value}'
+            },
+            lineWidth: 2
+        },
+        legend: {
+            enabled: false
+        },
+        tooltip: configureLineChartTooltip(),
+        plotOptions: {
+            spline: {
+                marker: {
+                    enable: false
+                }
+            }
+        },
+        series: [{
+            name: yName,
+            data: dataToShow
+        }]
+    });
+}
+
+function configureLineChartType() {
+    return {
+        type: 'spline',
+        inverted: true
+    };
+}
+
+function configureLineChartTooltip() {
+    return {
+        headerFormat: '<b>{series.name}</b><br/>',
+        pointFormat: '{point.x} km: {point.y}'
     };
 }
